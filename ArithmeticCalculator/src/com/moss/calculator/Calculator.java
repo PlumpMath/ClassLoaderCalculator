fb6b6ebc68fa77f29922b1ee9e366dc164b5005d
Marcin Ossowski <marcin.ossowski92@gmail.com>
2016-03-24 18:00:26 +0100

    Add calculators

---
+++ ArithmeticCalculator/src/com/moss/calculator/Calculator.java
@@ -0,0 +1,12 @@
+package com.moss.calculator;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class Calculator {
+
+	public static BigDecimal round(double number) {
+		return new BigDecimal(number).setScale(0, RoundingMode.HALF_UP);
+	}
+
+}
