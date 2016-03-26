47da17c3769766b6089bf67b9867665692afbc97
Marcin Ossowski <marcin.ossowski92@gmail.com>
2016-03-26 20:59:43 +0100

    Change WAR

---
+++ CalculatorWAR/src/main/java/com/moss/test/Test.java
@@ -0,0 +1,11 @@
+package com.moss.test;
+
+import java.math.BigDecimal;
+
+import com.moss.Calculator;
+
+public class Test {
+
+	public static double number = 2.45;
+	public static BigDecimal roundNumber = Calculator.round(2.45);
+}
---
+++ CalculatorWAR/src/main/webapp/index.jsp
@@ -0,0 +1,8 @@
+<%@page import="com.moss.test.Test"%>
+<html>
+<body>
+<h2>Calculator</h2>
+<% out.println("Number       : " + Test.number); %>
+<% out.println("Round number : " + Test.roundNumber); %>
+</body>
+</html>
